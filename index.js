4acb7c280080e12e01fddb2f27b1c5cd54e61f07
a-ruskomaa <48881971+a-ruskomaa@users.noreply.github.com>
2020-11-24 15:38:57 +0200

    Step 3.1 complete: Create express server that returns hardcoded data

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
-})
+let data = [
+    {
+      "name": "Arto Hellas",
+      "number": "040-123456",
+      "id": 1
+    },
+    {
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    {
+      "name": "Dan Abramov",
+      "number": "12-43-234345",
+      "id": 3
+    },
+    {
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122",
+      "id": 4
+    },
+    {
+      "name": "Aleksi",
+      "number": "345345",
+      "id": 6
+    }
+  ];
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+  app.get('/api/persons', (req, res) => {
+      res.json(data);
+  })
+
+  const PORT = 3001;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
